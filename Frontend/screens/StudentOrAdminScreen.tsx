9bc666b817dc17617adac0422147c8e44527d07f
Eyx2run <ajsenar86@gmail.com>
2025-04-20 21:10:49 +0800

    Update StudentOrAdminScreen.tsx

--- Frontend/screens/StudentOrAdminScreen.tsx
+++ Frontend/screens/StudentOrAdminScreen.tsx
@@ -46,7 +46,7 @@ export default function StudentScreen({ navigation }) {
           {/* Admin */}
           <View className="w-full">
             <CustomButton
-              onPress={() => handleLogin()}
+              onPress={() => handleLogin()} //Admin Registration pa
               title="Admin"
               backgroundColor="#FFFFFF"
               titleColor="#003A6C"
